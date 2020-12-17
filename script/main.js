8eceac3a227e438109178efbfdb6acc460ebbdbb
azamjon <azamjon.alikulov01@mail.ru>
2020-12-18 00:04:31 +0500

    first commit

---
+++ script/main.js
@@ -0,0 +1,11 @@
+var burgerButton = document.querySelector('#burger-button');
+var overlay = document.querySelector('#overlay'); 
+var body = document.querySelector('body');
+var mobileMenu = document.querySelector('#mobile_menu');
+
+burgerButton.addEventListener('click', function(e){
+  e.preventDefault();
+  this.classList.toggle('clicked')
+  overlay .classList.toggle('oppened')
+  mobileMenu .classList.toggle('oppen')
+});
